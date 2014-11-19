1e55147f27f5f48ee9ab4a8bfedb2414e7fc6e44
Ethan Urie <eurie@collective.com>
2014-11-19 13:13:43 -0500

    added in-place ruby quicksort

--- ruby/quicksort.rb
+++ ruby/quicksort.rb
@@ -19,5 +19,38 @@ module Sorting
     end
     quicksort(left) + pivot_a + quicksort(right)
   end
+
+  def self.in_place_quicksort(array, start_i = 0, end_i = nil)
+    end_i ||= array.length - 1
+
+    return array if start_i >= end_i
+
+    pivot_i = [((end_i - start_i)/2).floor, start_i].max
+    
+    left = start_i
+    right = end_i 
+
+    while left <= right 
+      while array[left] < array[pivot_i]
+        left += 1
+      end
+
+      while array[right] > array[pivot_i]
+        right -= 1
+      end
+
+      if left <= right
+        tmp = array[left]
+        array[left] = array[right]
+        array[right] = tmp
+
+        left += 1
+        right -= 1
+      end
+    end
+
+    in_place_quicksort(array, start_i, right)
+    in_place_quicksort(array, left, end_i)
+  end
 end
 
